d1b0a307a2dfb9477dc66f26c03d11a0323ecd2a
dangrouscs50 <57023375+dangrouscs50@users.noreply.github.com>
2022-01-06 10:58:36 -0800

    Exercise 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -13,29 +13,33 @@ const App = () => {
       <Button handleClick={() => setGood(good+1)} text="good" />
       <Button handleClick={() => setNeutral(neutral+1)} text="neutral" />
       <Button handleClick={() => setBad(bad+1)} text="bad" />
+      <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
 
 const Statistics = ({good, neutral, bad}) => {
-  return (
-    <>
-      <h1>statistics</h1>
-      <Readout value={good} text="good" />
-      <Readout value={neutral} text="neutral" />
-      <Readout value={bad} text="bad" />
-      <Readout value={good+neutral+bad} text="all" />
-      <Readout value={(good - bad)/(good+neutral+bad)} text="average" />
-      <Readout value={good/(good+neutral+bad)*100} text="positive" suffix="%" />
-    </>
-  )
+  if (good + neutral + bad == 0) {
+    return <div>No feedback given</div>
+  } else {  
+    return (
+      <>
+        <Readout value={good} text="good" />
+        <Readout value={neutral} text="neutral" />
+        <Readout value={bad} text="bad" />
+        <Readout value={good+neutral+bad} text="all" />
+        <Readout value={(good - bad)/(good+neutral+bad)} text="average" />
+        <Readout value={good/(good+neutral+bad)*100} text="positive" suffix="%" />
+      </>
+    )
+  }
 }
 
 const Button = ({handleClick, text}) =>
   <button onClick={handleClick}>{text}</button>
 
 const Readout = ({value, text, suffix}) =>
-  <p>{text} {value} {suffix}</p>
+  <div>{text} {value} {suffix}</div>
 
 export default App;
