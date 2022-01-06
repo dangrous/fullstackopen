4d8776f5eb4876ecb60df348d8c66f0e83fd8529
dangrouscs50 <57023375+dangrouscs50@users.noreply.github.com>
2022-01-06 10:53:09 -0800

    Exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,6 +5,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   return (
     <div>
@@ -13,9 +14,12 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral+1)} text="neutral" />
       <Button handleClick={() => setBad(bad+1)} text="bad" />
       <h1>statistics</h1>
-      <Readout feedback={good} text="good" />
-      <Readout feedback={neutral} text="neutral" />
-      <Readout feedback={bad} text="bad" />
+      <Readout value={good} text="good" />
+      <Readout value={neutral} text="neutral" />
+      <Readout value={bad} text="bad" />
+      <Readout value={good+neutral+bad} text="all" />
+      <Readout value={(good - bad)/(good+neutral+bad)} text="average" />
+      <Readout value={good/(good+neutral+bad)} text="positive" suffix="%" />
     </div>
   )
 }
@@ -23,7 +27,7 @@ const App = () => {
 const Button = ({handleClick, text}) =>
   <button onClick={handleClick}>{text}</button>
 
-const Readout = ({feedback, text}) =>
-  <p>{text} {feedback}</p>
+const Readout = ({value, text, suffix}) =>
+  <p>{text} {value} {suffix}</p>
 
 export default App;
