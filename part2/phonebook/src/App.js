97eac4c92a775d01e58170a23e7dd0f7e3878654
dangrouscs50 <57023375+dangrouscs50@users.noreply.github.com>
2022-01-10 10:32:33 -0800

    Exercise 2.9*

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,76 +1,85 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '39-44-5323523'
-    },
-  ]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [search, setSearch] = useState("");
 
   const addPerson = (event) => {
-    event.preventDefault()
-    let duplicate = false
-    // I think this can be done more elegantly and will
-    // need to be updated once there's more info in here...
+    event.preventDefault();
+    let duplicate = false;
+    // I think this can be done more elegantly and will need to be updated once there's more info in here...
     for (var i = 0; i < persons.length; i++) {
       if (newName === persons[i].name) {
-        duplicate = true
-        break
+        duplicate = true;
+        break;
       }
     }
     if (!duplicate) {
       const newPerson = {
         name: newName,
-        number: newNumber ? newNumber : "(no number provided)"
-      }
+        number: newNumber ? newNumber : "(no number provided)",
+      };
 
-      setPersons(persons.concat(newPerson))
-      setNewName('')
-      setNewNumber('')
+      setPersons(persons.concat(newPerson));
+      setNewName("");
+      setNewNumber("");
     } else {
-      alert(`${newName} is already added to phonebook`)
+      alert(`${newName} is already added to phonebook`);
     }
-  }
+  };
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
 
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-    console.log(event.target.value);
-  }
+    setNewNumber(event.target.value);
+  };
+
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={search} onChange={handleSearchChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input
-            value={newName}
-            onChange={handleNameChange}
-          />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          number: <input
-            value={newNumber}
-            onChange= {handleNumberChange}
-          />
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <Numbers persons={persons} />
+      <Numbers persons={persons} search={search} />
     </div>
-  )
-}
+  );
+};
 
-const Numbers = ({persons}) => persons.map((person) =>
-    <div key={person.name}>{person.name} {person.number}</div>)
+const Numbers = ({ persons, search }) =>
+  persons
+    .filter((person) =>
+      person.name.toLowerCase().includes(search.toLowerCase())
+    )
+    .map((person) => (
+      <div key={person.name}>
+        {person.name} {person.number}
+      </div>
+    ));
 
-export default App
+export default App;
