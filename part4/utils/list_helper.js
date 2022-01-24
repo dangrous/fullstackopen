c887b1963131b48205fdcbf4e770056c08278469
dangrous <5487802+dangrous@users.noreply.github.com>
2022-01-24 14:01:42 -0800

    Exercise 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const totalLikes = blogs.reduce((prev, cur) => prev + cur.likes, 0)
+  return totalLikes
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
