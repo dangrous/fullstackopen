e409cf999f93e72aa33996e861f3da68b9c688fd
dangrous <5487802+dangrous@users.noreply.github.com>
2022-01-25 12:36:51 -0800

    Exercise 4.8

---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  
+  const blogObjects = helper.initialBlogs
+    .map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+test.only('the correct number of blog posts is returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
