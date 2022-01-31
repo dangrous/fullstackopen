58b1d2981c239d772260b3abd29453ceeb06529b
dangrous <5487802+dangrous@users.noreply.github.com>
2022-01-31 15:20:09 -0800

    Exercise 5.6 and 5.7*

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,36 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, {useState} from 'react'
+
+const Blog = ({blog}) => {
+  const [visibility, setVisibility] = useState(false)
+
+  const toggleVisibility = () => {
+    setVisibility(!visibility)
+    console.log('switching visibility')
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const blogDetails = () => (
+    <>
+      <div>{blog.url}</div>
+      <div>likes {blog.likes} <button>like</button></div>
+      <div>{blog.user.name}</div>
+    </>
+  )
+  
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}{' '}
+      <button onClick={toggleVisibility}>{visibility ? 'hide' : 'view'}</button>
+      {visibility ? blogDetails() : ''}
+    </div>  
+  )
+}
 
 export default Blog
