3787fe2d0544a5c38ae83d0ad03ee639c242f517
dangrous <5487802+dangrous@users.noreply.github.com>
2022-02-01 14:41:44 -0800

    Exercise 5.11

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import PropTypes from 'prop-types'
 
 const Blog = ({blog, updateBlog, removeBlog, username}) => {
   const [visibility, setVisibility] = useState(false)
@@ -18,8 +19,9 @@ const Blog = ({blog, updateBlog, removeBlog, username}) => {
   }
 
   const deleteBlog = () => {
-    if (window.confirm(`Remove blog ${blog.title} by ${blog.author}?`))
-    removeBlog(blog.id)
+    if (window.confirm(`Remove blog ${blog.title} by ${blog.author}?`)) {
+      removeBlog(blog.id)
+    }
   }
 
   const blogStyle = {
@@ -50,6 +52,13 @@ const Blog = ({blog, updateBlog, removeBlog, username}) => {
       {visibility ? blogDetails() : ''}
     </div>  
   )
+}
+
+Blog.propTypes = {
+  blog: PropTypes.object.isRequired,
+  updateBlog: PropTypes.func.isRequired,
+  removeBlog: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired
 }
 
 export default Blog
