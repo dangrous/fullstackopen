923d7cb610d2cc997573ebc0f671e5f097b0a193
dangrous <5487802+dangrous@users.noreply.github.com>
2022-02-02 15:21:19 -0800

    Exercise 5.14*

--- bloglist-frontend/src/components/Blog.test.js
+++ bloglist-frontend/src/components/Blog.test.js
@@ -1,29 +1,44 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders content', () => {
-  const blog = {
-    title: 'This is a blog title',
-    author: 'Testing Author',
-    url: 'http://www.example.com',
-    likes: 69420,
-  }
+describe('<Blog /> component', () => {
+  let component
 
-  const component = render(
-    <Blog
-      blog={blog}
-      username='tester'
-      updateBlog={() => 0}
-      removeBlog={() => 0}
-    />
-  )
+  beforeEach(() => {
+    const blog = {
+      title: 'This is a blog title',
+      author: 'Testing Author',
+      url: 'http://www.example.com',
+      likes: 69420,
+      user: { username: 'tester' }
+    }
 
-  expect(component.container).toHaveTextContent('This is a blog title')
-  expect(component.container).toHaveTextContent('Testing Author')
-  expect(component.container).not.toHaveTextContent('http://www.example.com')
-  expect(component.container).not.toHaveTextContent('69420')
+    component = render(
+      <Blog
+        blog={blog}
+        username='tester'
+        updateBlog={() => 0}
+        removeBlog={() => 0}
+      />
+    )
+  })
 
-  component.debug()
+  test('renders author and title but not url or likes by default', () => {
+    expect(component.container).toHaveTextContent('This is a blog title')
+    expect(component.container).toHaveTextContent('Testing Author')
+    expect(component.container).not.toHaveTextContent('http://www.example.com')
+    expect(component.container).not.toHaveTextContent('69420')
+  })
+
+  test('clicking the view button shows url and likes', () => {
+    const button = component.getByText('view')
+    fireEvent.click(button)
+
+    expect(component.container).toHaveTextContent('This is a blog title')
+    expect(component.container).toHaveTextContent('Testing Author')
+    expect(component.container).toHaveTextContent('http://www.example.com')
+    expect(component.container).toHaveTextContent('69420')
+  })
 })
