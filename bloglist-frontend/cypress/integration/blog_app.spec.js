0f9ba0be6ae662c40e2a6e73ddf948f86e252b06
dangrous <5487802+dangrous@users.noreply.github.com>
2022-02-07 12:33:47 -0800

    Exercise 5.21*

--- bloglist-frontend/cypress/integration/blog_app.spec.js
+++ bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -60,7 +60,7 @@ describe('Blog app', function () {
       cy.contains('A cypress author')
     })
 
-    it.only('A blog can be liked', function () {
+    it('A blog can be liked', function () {
       cy.contains('create new blog').click()
       cy.get('#title').type('A cypress blog title')
       cy.get('#author').type('A cypress author')
@@ -71,6 +71,54 @@ describe('Blog app', function () {
       cy.contains('likes 0')
       cy.contains('A cypress blog title').parent().find('.like-button').click()
       cy.contains('likes 1')
+    })
+
+    it('The user who created a blog can delete it', function () {
+      cy.contains('create new blog').click()
+      cy.get('#title').type('A cypress blog title')
+      cy.get('#author').type('A cypress author')
+      cy.get('#url').type('google.com')
+      cy.get('#blog-button').click()
+
+      cy.contains('view').click()
+      cy.contains('remove').click()
+
+      cy.should('not.contain', 'A cypress blog title')
+    })
+
+    it.only('A user who did not create a blog cannot delete it', function () {
+      cy.contains('create new blog').click()
+      cy.get('#title').type('A cypress blog title')
+      cy.get('#author').type('A cypress author')
+      cy.get('#url').type('google.com')
+      cy.get('#blog-button').click()
+
+      cy.contains('A cypress blog title')
+      cy.contains('A cypress author')
+      cy.contains('view').click()
+      cy.contains('remove')
+
+      const user = {
+        name: 'Cypress Tester Two',
+        username: 'cypress2',
+        password: 'cypresstesting2',
+      }
+      cy.request('POST', 'http://localhost:3003/api/users/', user)
+
+      cy.contains('logout').click()
+
+      cy.request('POST', 'http://localhost:3003/api/login', {
+        username: 'cypress2',
+        password: 'cypresstesting2',
+      }).then(({ body }) => {
+        localStorage.setItem('loggedBlogappUser', JSON.stringify(body))
+        cy.visit('http://localhost:3000')
+      })
+
+      cy.contains('A cypress blog title')
+      cy.contains('A cypress author')
+      cy.contains('view').click()
+      cy.should('not.contain', 'remove')
     })
   })
 })
